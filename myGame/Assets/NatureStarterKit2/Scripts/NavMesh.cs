d10b6a31c7d6585aba221182d7fb97f6d825d419
RomanovArtem <temik_roanov@mail.ru>
2018-03-27 00:39:25 +0300

    Добавил расчет пути от врага до цели

---
+++ myGame/Assets/NatureStarterKit2/Scripts/NavMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+
+    public Transform target; // куда будет идти враг
+    NavMeshAgent agent;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
