eebfa3cf09f43e9c243129515363b5e9f436e694
RomanovArtem <temik_roanov@mail.ru>
2018-03-27 04:15:33 +0300

    Сделал анимацию для врагов Убрал отображение курсора во время игры

--- myGame/Assets/NatureStarterKit2/Scripts/NavMesh.cs
+++ myGame/Assets/NatureStarterKit2/Scripts/NavMesh.cs
@@ -8,17 +8,38 @@ public class NavMesh : MonoBehaviour
 
     public Transform target; // куда будет идти враг
     NavMeshAgent agent;
-
+    Animator animator;
 
     // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.position);
+
+        ChangeSpeed();
+    }
+
+    /// <summary>
+    /// изменение скорости аниматора и бега
+    /// </summary>
+    void ChangeSpeed()
+    {
+        var direction = target.position - agent.nextPosition;
+        if (direction.magnitude > 20)
+        {
+            animator.SetFloat("Speed", 4);
+            agent.speed = 8;
+        }
+        else {
+            animator.SetFloat("Speed", 2.1f);
+            agent.speed = 4;
+        }
     }
 }
